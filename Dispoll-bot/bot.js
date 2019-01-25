3079fc13882c8331771aea58d6f7f1662286944c
Hugo Torzuoli <torzuoli.hugo@gmail.com>
2019-01-25 23:04:25 +0100

    Embed builder

--- Dispoll-bot/bot.js
+++ Dispoll-bot/bot.js
@@ -6,17 +6,34 @@ const http = require('http').Server(app)
 const io = require('socket.io')(http)
 const config = require('./config')
 
+const digimojis = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣'];
+
 const client = new Discord.Client()
 client.on('ready', () => {
     console.log('Bot is ready!')
 })
 client.login(config.discordClientID);
 
+function buildChoices(choices) {
+    let choicesStringValue = '';
+    choices.forEach((choice, iterator) => {
+        choicesStringValue += `${digimojis[iterator]} ${choice}\n`
+    })
+    return choicesStringValue;
+}
+
+function buildEmbed(question, choices) {
+    return new Discord.RichEmbed()
+        .setTitle('A new poll arrived!')
+        .setColor('#00AE86')
+        .addField(question, buildChoices(choices))
+}
+
 io.on('connection', socket => {
     console.log('Socket connected !')
     socket.on('newPoll', poll => {
         console.log('Received new poll', poll)
-        client.channels.get(poll.discordID).send(poll.question)
+        client.channels.get(poll.discordID).send(buildEmbed(poll.question, poll.choices))
     })
 })
 http.listen(3333)
